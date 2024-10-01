6f3918423e98fbf29d17b5924a30af20d18e1556
tanpmqe180124 <tanpmqe180124@fpt.edu.vn>
2024-10-01 17:14:45 +0700

    first

---
+++ lab3/src/App.js
@@ -0,0 +1,172 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Carousel from 'react-bootstrap/Carousel';
+import pizza1 from './Images/pizza1.jpg'
+import pizza2 from './Images/pizza2.jpg'
+import pizza3 from './Images/pizza3.jpg'
+import { Col, Row } from 'react-bootstrap';
+import Card from 'react-bootstrap/Card';
+import menu1 from './Images/menu1.jpg'
+import menu2 from './Images/menu2.jpg'
+import menu3 from './Images/menu3.jpg'
+import menu4 from './Images/menu4.jpg'
+import Badge from 'react-bootstrap/Badge';
+import { useState } from 'react';
+import Modal from 'react-bootstrap/Modal';
+
+function App() {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  return (
+    <Container>
+      <Row>
+      <Navbar expand="lg" className="bg-body-tertiary">
+      <Container fluid>
+        <Navbar.Brand href="#">Pizza House</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: '100px' }}
+            navbarScroll
+          >
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">About Us</Nav.Link>
+            <Nav.Link href="#action3">Contact</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
+            
+          </Form>
+          <Button variant="primary" onClick={handleShow}>
+      Items: <Badge bg="secondary">0</Badge>
+      <span className="visually-hidden">unread messages</span>
+    </Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+      </Row>
+
+      <Row>
+      <Carousel>
+      <Carousel.Item>
+        <img text="First slide" src={pizza1}/>
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img text="First slide" src={pizza2}/>
+      <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img text="First slide" src={pizza3}/>
+      <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+      </Row>
+      <Row className='mt-3'>
+
+      </Row>
+      <Row>
+        <Col>
+        <Card>
+      <Card.Img variant="top" src= {menu1} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+        <Col>
+        <Card>
+      <Card.Img variant="top" src= {menu2} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+        <Col>
+        <Card>
+      <Card.Img variant="top" src= {menu3} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+        <Col>
+        <Card>
+      <Card.Img variant="top" src= {menu4} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+        </Col>
+      </Row>
+      <Row>
+      <>
+
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+      </Row>
+    </Container>
+  );
+}
+
+export default App;
